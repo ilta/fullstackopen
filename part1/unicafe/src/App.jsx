26a9d1eafacff746f1ece7cccf7cdbea5eae2ae1
Ilkka Tommola <ilta@iki.fi>
2025-01-07 17:07:24 +0200

    Completed ex 1.9

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -8,15 +8,25 @@ const Statistics = ({ good, neutral, bad }) => {
   return (
     <div>
       <h1>statistics</h1>
-      <p>good: {good}</p>
-      <p>neutral: {neutral}</p>
-      <p>bad: {bad}</p>
-      <p>all: {totalClicks}</p>
-      <p>average: {average ? average : 0}</p>
-      <p>positive: {positiveRatio ? positiveRatio : 0} %</p>
+      {totalClicks ? (
+        <>
+          <p>good: {good}</p>
+          <p>neutral: {neutral}</p>
+          <p>bad: {bad}</p>
+          <p>all: {totalClicks}</p>
+          <p>average: {average ? average : 0}</p>
+          <p>positive: {positiveRatio ? positiveRatio : 0} %</p>
+        </>
+      ) : (
+        'No feedback given'
+      )}
     </div>
   );
 };
+
+const Button = ({ handleClick, text }) => (
+  <button onClick={handleClick}>{text}</button>
+);
 
 const App = () => {
   // save clicks of each button to its own state
@@ -27,10 +37,6 @@ const App = () => {
   const handleGoodClick = () => setGood(good + 1);
   const handleBadClick = () => setBad(bad + 1);
   const handleNeutralClick = () => setNeutral(neutral + 1);
-
-  const Button = ({ handleClick, text }) => (
-    <button onClick={handleClick}>{text}</button>
-  );
 
   return (
     <div>
