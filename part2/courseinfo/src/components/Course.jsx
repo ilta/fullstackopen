e00da97d9e9a2ff3db455272f6d5ffff2c7f92d8
Ilkka Tommola <ilta@iki.fi>
2025-01-07 22:42:32 +0200

    Completed ex 2.4 and 2.5
    Notes:
    * Exercise 2.5 was already done
    * Using separate modules even though that was not required

--- part2/courseinfo/src/components/Course.jsx
+++ part2/courseinfo/src/components/Course.jsx
@@ -2,12 +2,16 @@ import Content from './Content';
 import Header from './Header';
 import Total from './Total';
 
-const Course = ({ course }) => {
+const Course = ({ courses }) => {
   return (
     <>
-      <Header name={course.name} />
-      <Content parts={course.parts} />
-      <Total parts={course.parts} />
+      {courses.map((course) => (
+        <div key={course.id}>
+          <Header name={course.name} />
+          <Content parts={course.parts} />
+          <Total parts={course.parts} />
+        </div>
+      ))}
     </>
   );
 };
