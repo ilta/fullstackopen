e34defe46d36d11008bb702b6a68e602ff48c239
Ilkka Tommola <ilta@iki.fi>
2025-01-07 16:20:17 +0200

    Completed ex 1.7

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -6,9 +6,13 @@ const App = () => {
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
 
+  const totalClicks = good + neutral + bad;
+  const average = (good + -1 * bad) / totalClicks;
+  const positiveRatio = (good / totalClicks) * 100;
+
   const handleGoodClick = () => setGood(good + 1);
-  const handleNeutralClick = () => setBad(bad + 1);
-  const handleBadClick = () => setNeutral(neutral + 1);
+  const handleBadClick = () => setBad(bad + 1);
+  const handleNeutralClick = () => setNeutral(neutral + 1);
 
   const Button = ({ handleClick, text }) => (
     <button onClick={handleClick}>{text}</button>
@@ -18,12 +22,15 @@ const App = () => {
     <div>
       <h1>give feedback</h1>
       <Button handleClick={handleGoodClick} text="good" />
-      <Button handleClick={handleBadClick} text="bad" />
       <Button handleClick={handleNeutralClick} text="neutral" />
+      <Button handleClick={handleBadClick} text="bad" />
       <h1>statistics</h1>
       <p>good: {good}</p>
-      <p>bad: {bad}</p>
       <p>neutral: {neutral}</p>
+      <p>bad: {bad}</p>
+      <p>all: {totalClicks}</p>
+      <p>average: {average ? average : 0}</p>
+      <p>positive: {positiveRatio ? positiveRatio : 0} %</p>
     </div>
   );
 };
