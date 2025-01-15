a89da2cec00f9d889667e0dbf7b2ffaa372007c3
Ilkka Tommola <ilta@iki.fi>
2025-01-15 19:15:32 +0200

    Completed ex 4.5

--- part4/bloglist-backend/utils/list_helper.js
+++ part4/bloglist-backend/utils/list_helper.js
@@ -10,7 +10,23 @@ const totalLikes = (blogs) => {
   return blogs.reduce(reducer, 0)
 }
 
+const favoriteBlog = (blogs) => {
+  if (blogs.length === 0) return {}
+
+  // See e.g. https://seanconnolly.dev/javascript-find-element-with-max-value
+  const favorite = blogs.reduce((prev, current) => {
+    return prev.likes > current.likes ? prev : current
+  })
+
+  return {
+    title: favorite.title,
+    author: favorite.author,
+    likes: favorite.likes,
+  }
+}
+
 module.exports = {
   dummy,
   totalLikes,
+  favoriteBlog,
 }
