b8e404c0baa776f4450a07e801716c86bc337fdb
Ilkka Tommola <ilta@iki.fi>
2025-01-15 21:26:16 +0200

    Refactoring

--- part4/bloglist-backend/tests/list_helper.test.js
+++ part4/bloglist-backend/tests/list_helper.test.js
@@ -1,7 +1,6 @@
 const { test, describe } = require('node:test')
 const assert = require('node:assert')
 const listHelper = require('../utils/list_helper')
-const { result } = require('lodash')
 
 const blogs = [
   {
@@ -54,80 +53,82 @@ const blogs = [
   },
 ]
 
-test('dummy returns one', () => {
-  const blogs = []
+describe('list_helper', () => {
+  test('dummy returns one', () => {
+    const blogs = []
 
-  const result = listHelper.dummy(blogs)
-  assert.strictEqual(result, 1)
-})
-
-describe('total likes', () => {
-  const listWithOneBlog = [
-    {
-      _id: '5a422aa71b54a676234d17f8',
-      title: 'Go To Statement Considered Harmful',
-      author: 'Edsger W. Dijkstra',
-      url: 'https://homepages.cwi.nl/~storm/teaching/reader/Dijkstra68.pdf',
-      likes: 5,
-      __v: 0,
-    },
-  ]
-
-  test('when list has only one blog, equals the likes of that', () => {
-    const result = listHelper.totalLikes(listWithOneBlog)
-    assert.strictEqual(result, 5)
-  })
-})
-
-describe('favorite blog', () => {
-  test('favorite blog is returned in JSON format', () => {
-    const expectedResult = {
-      title: 'Canonical string reduction',
-      author: 'Edsger W. Dijkstra',
-      likes: 12,
-    }
-    const result = listHelper.favoriteBlog(blogs)
-    assert.deepStrictEqual(result, expectedResult)
-  })
-})
-
-describe('most blogs', () => {
-  test('author with most blog post is returned in JSON format', () => {
-    const expectedResult = {
-      author: 'Robert C. Martin',
-      blogs: 3,
-    }
-    const result = listHelper.mostBlogs(blogs)
-    assert.deepStrictEqual(result, expectedResult)
-  })
-})
-
-describe('findMaxByKey', () => {
-  test('returns an empty object if when there are no blogs', () => {
-    const result = listHelper.findMaxByKey([], '')
-    assert.deepStrictEqual(result, {})
+    const result = listHelper.dummy(blogs)
+    assert.strictEqual(result, 1)
   })
 
-  test('returns the object with the highest value in the example case', () => {
-    const testArray = [
+  describe('total likes', () => {
+    const listWithOneBlog = [
       {
-        name: 'Bob',
-        salary: 5000,
-      },
-      {
-        name: 'Paula',
-        salary: 6500,
-      },
-      {
-        name: 'Jack',
-        salary: 4500,
+        _id: '5a422aa71b54a676234d17f8',
+        title: 'Go To Statement Considered Harmful',
+        author: 'Edsger W. Dijkstra',
+        url: 'https://homepages.cwi.nl/~storm/teaching/reader/Dijkstra68.pdf',
+        likes: 5,
+        __v: 0,
       },
     ]
-    const expectedResult = {
-      name: 'Paula',
-      salary: 6500,
-    }
-    const result = listHelper.findMaxByKey(testArray, 'salary')
-    assert.deepStrictEqual(result, expectedResult)
+
+    test('when list has only one blog, equals the likes of that', () => {
+      const result = listHelper.totalLikes(listWithOneBlog)
+      assert.strictEqual(result, 5)
+    })
+  })
+
+  describe('favorite blog', () => {
+    test('favorite blog is returned', () => {
+      const expectedResult = {
+        title: 'Canonical string reduction',
+        author: 'Edsger W. Dijkstra',
+        likes: 12,
+      }
+      const result = listHelper.favoriteBlog(blogs)
+      assert.deepStrictEqual(result, expectedResult)
+    })
+  })
+
+  describe('most blogs', () => {
+    test('author with most blog post is returned', () => {
+      const expectedResult = {
+        author: 'Robert C. Martin',
+        blogs: 3,
+      }
+      const result = listHelper.mostBlogs(blogs)
+      assert.deepStrictEqual(result, expectedResult)
+    })
+  })
+
+  describe('findMaxByKey', () => {
+    test('returns an empty object if when there are no blogs', () => {
+      const result = listHelper.findMaxByKey([], '')
+      assert.deepStrictEqual(result, {})
+    })
+
+    test('returns the object with the highest value in the example case', () => {
+      const testArray = [
+        {
+          name: 'Bob',
+          salary: 5000,
+        },
+        {
+          name: 'Paula',
+          salary: 6500,
+        },
+        {
+          name: 'Jack',
+          salary: 4500,
+        },
+      ]
+      const expectedResult = {
+        name: 'Paula',
+        salary: 6500,
+      }
+      const result = listHelper.findMaxByKey(testArray, 'salary')
+      assert.deepStrictEqual(result, expectedResult)
+    })
   })
 })
