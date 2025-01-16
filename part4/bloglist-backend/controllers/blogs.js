71d0300e6db9c3fd0ddaa2995ee8d5a0e6d4e098
Ilkka Tommola <ilta@iki.fi>
2025-01-16 17:58:13 +0200

    Completed ex 4.8

--- part4/bloglist-backend/controllers/blogs.js
+++ part4/bloglist-backend/controllers/blogs.js
@@ -1,19 +1,17 @@
 const blogRouter = require('express').Router()
 const Blog = require('../models/blog')
 
-blogRouter.get('/', (_request, response) => {
-  Blog.find({}).then((blogs) => {
-    response.json(blogs)
-  })
+blogRouter.get('/', async (_request, response) => {
+  const blogs = await Blog.find({})
+  response.json(blogs)
 })
 
-blogRouter.post('/', (request, response) => {
+blogRouter.post('/', async (request, response) => {
   const { title, author, url, likes } = request.body
   const blog = new Blog({ title, author, url, likes })
 
-  blog.save().then((result) => {
-    response.status(201).json(result)
-  })
+  const savedBlog = await blog.save()
+  response.status(201).json(savedBlog)
 })
 
 module.exports = blogRouter
---
+++ part4/bloglist-backend/tests/blogs_api.test.js
@@ -0,0 +1,37 @@
+const { test, after, beforeEach, describe } = require('node:test')
+const assert = require('node:assert')
+const mongoose = require('mongoose')
+const supertest = require('supertest')
+const app = require('../app')
+const api = supertest(app)
+
+const Blog = require('../models/blog')
+const helper = require('../utils/test_helper')
+
+beforeEach(async () => {
+  await Blog.deleteMany({})
+
+  for (let blog of helper.initialBlogs) {
+    let blogObject = new Blog(blog)
+    await blogObject.save()
+  }
+})
+
+describe('blogs_api', () => {
+  test('blogs are returned as json', async () => {
+    await api
+      .get('/api/blogs')
+      .expect(200)
+      .expect('Content-Type', /application\/json/)
+  })
+
+  test('there are three blog posts', async () => {
+    const response = await api.get('/api/blogs')
+
+    assert.strictEqual(response.body.length, 3)
+  })
+
+  after(async () => {
+    await mongoose.connection.close()
+  })
+})
