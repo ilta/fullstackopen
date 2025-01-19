e6d14cbb037289571053d17e54e85f277855dbbe
Ilkka Tommola <ilta@iki.fi>
2025-01-19 17:37:35 +0200

    Completed ex 4.15

--- part4/bloglist-backend/app.js
+++ part4/bloglist-backend/app.js
@@ -3,6 +3,7 @@ const express = require('express')
 const app = express()
 exports.app = app
 const cors = require('cors')
+const usersRouter = require('./controllers/users')
 const blogRouter = require('./controllers/blogs')
 const middleware = require('./utils/middleware')
 const mongoose = require('mongoose')
@@ -13,6 +14,7 @@ mongoose.connect(mongoUrl)
 app.use(cors())
 app.use(express.json())
 
+app.use('/api/users', usersRouter)
 app.use('/api/blogs', blogRouter)
 
 app.use(middleware.unknownEndpoint)
