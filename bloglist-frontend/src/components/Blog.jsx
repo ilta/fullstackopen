c62ee05347600f45ace3db0159103e4a186ed2e3
Ilkka Tommola <ilta@iki.fi>
2025-01-24 18:19:29 +0200

    Completed ex 5.7

--- bloglist-frontend/src/components/Blog.jsx
+++ bloglist-frontend/src/components/Blog.jsx
@@ -1,7 +1,47 @@
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import { useState } from 'react'
+
+const blogStyle = {
+  paddingTop: 10,
+  paddingLeft: 2,
+  marginRight: '8em',
+  border: 'solid',
+  borderWidth: 1,
+  marginBottom: 5,
+}
+
+const Blog = ({ blog }) => {
+  const [expandedView, setExpandedView] = useState(false)
+
+  const toggleExpandedView = () => {
+    setExpandedView(!expandedView)
+  }
+
+  if (expandedView) {
+    return (
+      <div style={blogStyle}>
+        <div>
+          {blog.title} {blog.author}
+        </div>
+        <div>{blog.url}</div>
+        <div>
+          likes {blog.likes} <button>like</button>
+        </div>
+        <div>{blog.user.name}</div>
+        <button name="expandButton" onClick={toggleExpandedView}>
+          hide
+        </button>
+      </div>
+    )
+  }
+
+  return (
+    <div style={blogStyle}>
+      {blog.title} {blog.author}
+      <button name="expandButton" onClick={toggleExpandedView}>
+        view
+      </button>
+    </div>
+  )
+}
 
 export default Blog
