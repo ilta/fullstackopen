96b0293b603b4e3b56c220210830eb5b11cda9ad
Ilkka Tommola <ilta@iki.fi>
2025-02-06 22:33:02 +0200

    Completed ex 7.8

--- part7/ultimate-hooks/src/App.jsx
+++ part7/ultimate-hooks/src/App.jsx
@@ -8,35 +8,63 @@ const useField = (type) => {
     setValue(event.target.value)
   }
 
+  const reset = () => {
+    setValue('')
+  }
+
   return {
     type,
     value,
-    onChange
+    onChange,
+    reset,
   }
 }
 
 const useResource = (baseUrl) => {
   const [resources, setResources] = useState([])
 
-  // ...
+  let token = null
+
+  // eslint-disable-next-line no-unused-vars
+  const setToken = (newToken) => {
+    token = `bearer ${newToken}`
+  }
+
+  useEffect(() => {
+    const request = axios.get(baseUrl)
+    request.then((response) => setResources(response.data))
+  }, [baseUrl])
 
   const create = (resource) => {
-    // ...
+    const config = {
+      headers: { Authorization: token },
+    }
+
+    const request = axios.post(baseUrl, resource, config)
+    request.then((response) => setResources(resources.concat(response.data)))
   }
 
   const service = {
-    create
+    create,
   }
 
-  return [
-    resources, service
-  ]
+  return [resources, service]
+}
+
+// A custom input component that discards unknown attribute "reset" and adds
+// attribute "name" to enable form auto-fill etc. Attribute "type" is also
+// discarded because setting the type in specific cases, e.g. "number", changes
+// the input method, also disallowing inputting strings.
+
+// eslint-disable-next-line react/prop-types, no-unused-vars
+const Input = ({ type, reset, ...rest }) => {
+  return <input {...rest} name={type} />
 }
 
 const App = () => {
-  const content = useField('text')
-  const name = useField('text')
-  const number = useField('text')
+  const content = useField('content')
+  const name = useField('name')
+  const number = useField('number')
 
   const [notes, noteService] = useResource('http://localhost:3005/notes')
   const [persons, personService] = useResource('http://localhost:3005/persons')
@@ -44,29 +72,38 @@ const App = () => {
   const handleNoteSubmit = (event) => {
     event.preventDefault()
     noteService.create({ content: content.value })
+    content.reset()
   }
- 
+
   const handlePersonSubmit = (event) => {
     event.preventDefault()
-    personService.create({ name: name.value, number: number.value})
+    personService.create({ name: name.value, number: number.value })
+    name.reset()
+    number.reset()
   }
 
   return (
     <div>
       <h2>notes</h2>
       <form onSubmit={handleNoteSubmit}>
-        <input {...content} />
+        <Input {...content} required />
         <button>create</button>
       </form>
-      {notes.map(n => <p key={n.id}>{n.content}</p>)}
+      {notes.map((n) => (
+        <p key={n.id}>{n.content}</p>
+      ))}
 
       <h2>persons</h2>
       <form onSubmit={handlePersonSubmit}>
-        name <input {...name} /> <br/>
-        number <input {...number} />
+        name <Input {...name} required /> <br />
+        number <Input {...number} required />
         <button>create</button>
       </form>
-      {persons.map(n => <p key={n.id}>{n.name} {n.number}</p>)}
+      {persons.map((n) => (
+        <p key={n.id}>
+          {n.name} {n.number}
+        </p>
+      ))}
     </div>
   )
 }
